3be9d5ebb81bf7b8150f33ae4fa7c23685ac88c1
timothypeter <timothy_peter@yahoo.com>
2017-09-26 21:32:26 -0700

    Added comments to SplitViewController

--- ProCore GitHub Project/Controller/SplitViewController.swift
+++ ProCore GitHub Project/Controller/SplitViewController.swift
@@ -19,15 +19,18 @@ class SplitViewController: UIViewController{
         super.viewDidLoad()
     }
     
+    //Do some work before we lead each of our segues that are inside the container views
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         if(segue.identifier == "SHOWDIFFVC"){
             let diffViewController: DiffViewController = segue.destination as! DiffViewController
+            //set the diff info to load
             diffViewController.diffInfoAsJSON = self.diffInfoAsJSON
         }
         
         else
         {
             let leftViewController: LeftViewController = segue.destination as! LeftViewController
+            //set the diff info to load
             leftViewController.diffInfoAsJSON = self.diffInfoAsJSON
         }
     }
